0aef07686c3d28fffe19fcb32e5e3dc26e098dea
passosleo <leonardopassos98@gmail.com>
2022-05-13 19:18:22 -0300

    feat: modal

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -2,6 +2,7 @@ $(".menu-button").click(openMenu);
 
 function openMenu() {
   const menu = $(".menu-vertical");
+
   if (!menu.hasClass("visible")) {
     menu.addClass("visible");
     menu.fadeIn();
