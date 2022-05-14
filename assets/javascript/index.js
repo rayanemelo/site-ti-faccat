525c4d927b5ed8ae2a1f001b6ff584f59a7dc55a
passosleo <leonardopassos98@gmail.com>
2022-05-14 17:54:09 -0300

    bootstrap components

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -1,4 +1,7 @@
 $(".menu-button").click(openMenu);
+$(".modal-matriz").click(openModal);
+$(".close-modal").click(openModal);
+$(".fechar-modal").click(openModal);
 
 function openMenu() {
   const menu = $(".menu-vertical");
@@ -11,3 +14,21 @@ function openMenu() {
     menu.fadeOut();
   }
 }
+
+// function openModal() {
+//   const modal = $(".modal");
+
+//   if (!modal.hasClass("visible")) {
+//     modal.addClass("visible");
+//     modal.fadeIn();
+//     $("body").addClass("stop-scrolling");
+//     $("body").bind("touchmove", function (e) {
+//       e.preventDefault();
+//     });
+//   } else {
+//     modal.removeClass("visible");
+//     modal.fadeOut();
+//     $("body").removeClass("stop-scrolling");
+//     $("body").unbind("touchmove");
+//   }
+// }
